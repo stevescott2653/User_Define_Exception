ffd6a1ed28475efe23c1dac441d9a249472a18d8
Steven Scott <stevescott2653@gmail.com>
2019-10-02 11:36:26 -0400

    Add files via upload

---
+++ User_Define_Exception/User_Define_Exception/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Define_Exception
+{
+    public class ProgramInvalidAgeException : Exception
+    {
+        public ProgramInvalidAgeException(String message)
+            : base(message)
+        {
+
+        }
+    }
+
+    public class TestUserDefinedException
+    {
+        static void validate(int age)
+        {
+            if (age < 18)
+            {
+                throw new ProgramInvalidAgeException("Sorry, Age must be greater than 18");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                validate(12);
+            }
+            catch (ProgramInvalidAgeException e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine("Rest of the code");
+        }
+    }
+}
